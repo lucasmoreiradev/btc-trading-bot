9e3bedb83c2cec0c1339d47a6fd5352534004c66
Lucas Moreira <eskerlucas1@gmail.com>
2018-05-28 23:55:28 -0300

    Implement trade api

---
+++ MercadoBitcoinTrade.js
@@ -0,0 +1,65 @@
+const unirest = require('unirest')
+const qs = require('querystring')
+const crypto = require('crypto')
+const ENDPOINT_API = 'https://www.mercadobitcoin.com.br/api/'
+const ENDPOINT_TRADE_PATH = "/tapi/v3/"
+const ENDPOINT_TRADE_API = 'https://www.mercadobitcoin.net' + ENDPOINT_TRADE_PATH
+
+class MercadoBitcoinTrade {
+  constructor (config) {
+    this.currency = config.currency 
+    this.key = config.key
+    this.secret = config.secret
+    this.pin = config.pin
+  }
+  getAccountInfo (success, error) {
+    this.call('get_account_info', {}, success, error)
+  }
+  listMyOrders (parameters, success, error) {
+    this.call('list_orders', parameters, success, error)
+  }
+  placeBuyOrder (qty, limit_price, success, error) {
+    this.call('place_buy_order', { coin_pair: `BRL${this.currency}`, 
+      quantity: (''+qty).substr(0,10), limit_price: ''+limit_price }, success, error)
+  }
+  placeSellOrder (qty, limit_price, success, error) {
+    this.call('place_sell_order', { coin_pair: `BRL${this.currency}`,
+      quantity: (''+qty).substr(0,10), limit_price: ''+limit_price }, success, error)
+  }
+  cancelOrder (orderId, success, error) {
+    this.call('cancel_order', { coin_pair: `BRL${this.currency}`,
+      order_id: orderId }, success, error)
+  }
+  call (method, parameters, success, error) {
+   const now = Math.round(new Date().getTime() / 1000)
+   let queryString = qs.stringify({ 'tapi_method': method, 'tapi_nonce': now })
+    if (parameters) {
+      queryString += '&' + qs.stringify(parameters)
+    } 
+ 
+   const signature = crypto.createHmac('sha512', this.secret)
+                         .update(ENDPOINT_TRADE_PATH + '?' + queryString)
+                         .digest('hex')
+ 
+   unirest.post(ENDPOINT_TRADE_API)
+          .headers({ 'TAPI-ID': this.key })
+          .headers({ 'TAPI-MAC': signature })
+          .send(queryString)
+          .end(response => {
+              if (response.body) {
+                if (response.body.status_code === 100 && success) {
+                  success(response.body.response_data)
+                } else if (error) {
+                  error(response.body.error_message)
+                } else {
+                  console.log(response.body)
+                }
+              } else {
+                console.log(response)
+              }
+          })
+
+  }
+}
+
+module.exports = MercadoBitcoinTrade
--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 require("dotenv-safe").load()
+const chalk = require('chalk')
 const MercadoBitcoin = require('./MercadoBitcoin')
+const MercadoBitcoinTrade = require('./MercadoBitcoinTrade')
 const infoApi = new MercadoBitcoin({ currency: 'BTC' })
+const tradeApi = new MercadoBitcoinTrade({
+  currency: 'BTC',
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  pin: process.env.PIN
+})
 
 setInterval(() => 
-  infoApi.ticker(tick => console.log(tick.ticker)),
+  infoApi.ticker(response => {
+    const priceToBuy = 25000
+    if (response.ticker.sell <= priceToBuy) {
+      getQuantity('BRL', response.ticker.sell, true, (qty) => {
+        tradeApi.placeBuyOrder(qty, response.ticker.sell, 
+          (data) => {
+            console.log(chalk.green(`Buy order: ${data}`))
+            //operando em STOP
+            tradeApi.placeSellOrder(data.quantity, response.ticker.sell * parseFloat(process.env.PROFITABILITY), 
+              (data) => console.log(chalk.green(`Sell order: ${data}`)),
+              (data) => console.log(chalk.red(`Error sell order: ${data}`)))
+          },
+          (data) => console.log(chalk.red(`Error buy order: ${data}`)))
+      })
+    } else {
+      console.log(chalk.yellow(`${response.ticker.sell} is greater than ${priceToBuy}... trying again in ${process.env.CRAWLER_INTERVAL/1000} seconds`))
+    }
+  }),
   process.env.CRAWLER_INTERVAL
 )
+
+function getQuantity(coin, price, isBuy, callback) {
+  price = parseFloat(price)
+  coin = isBuy ? 'brl' : coin.toLowerCase()
+
+  tradeApi.getAccountInfo(response_data => {
+    let balance = parseFloat(response_data.balance[coin].available).toFixed(5)
+    balance = parseFloat(balance)
+    if (isBuy && balance < 50) { 
+      return console.log('Sem saldo disponível para comprar!')
+    }
+    console.log(`Saldo disponível de ${coin}: ${balance}`)
+
+    if (isBuy) {
+      balance = parseFloat((balance / price).toFixed(5))
+    }
+    callback(parseFloat(balance) - 0.00001) //tira a diferença que se ganha no arredondamento
+  }, 
+  (data) => console.log(data))
+}
